4aaa2e9e1d9dd968c96e3b64e2d18c54c7dc4012
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2017-02-10 10:49:02 +0100

    Folder name

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ export { default as CustomSelect } from './components/ui/CustomSelect';
 export { default as Icon } from './components/ui/Icon';
 export { default as Radio } from './components/ui/Radio';
 export { default as RadioGroup } from './components/ui/RadioGroup';
-export { default as SegmentedUi } from './components/ui/SegmentedUI';
+export { default as SegmentedUI } from './components/ui/SegmentedUI';
 export { default as Timeline } from './components/ui/Timeline';
 export { default as Spinner } from './components/ui/Spinner';
 // Reducers
