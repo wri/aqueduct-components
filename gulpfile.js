880e38d4d3c219cc7abdb1c898cc632519283f74
Óscar Ferrándiz <oscar.ferrandiz@vizzuality.com>
2017-02-15 18:21:06 +0100

    Bundle removed

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const del = require('del');
+
+// Dir config
+const CONFIG = {
+  dist: 'lib'
+};
+
+gulp.task('clean', () => {
+  del.sync(CONFIG.dist);
+});
+
+gulp.task('dist', ['clean'], () => {
+  gulp.src('src/**/*.{js,jsx}')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest(CONFIG.dist));
+});
+
+gulp.task('dev', () => {
+  gulp.watch('src/**/*.{js,jsx}', ['dist']);
+});
