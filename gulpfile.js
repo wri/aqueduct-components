8694dd966c5b15e5893984adb2ed31cc5e8de56d
Óscar Ferrándiz <oscar.ferrandiz@vizzuality.com>
2017-02-21 15:47:39 +0100

    SVG icon sprite added

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,35 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const del = require('del');
 
-// Dir config
+/* Dir config */
+
 const CONFIG = {
   dist: 'lib'
 };
+
+
+/* Partial tasks */
 
 gulp.task('clean', () => {
   del.sync(CONFIG.dist);
 });
 
-gulp.task('dist', ['clean'], () => {
+gulp.task('html', () => {
+  gulp.src('src/**/*.html')
+  .pipe(gulp.dest(CONFIG.dist));
+});
+
+gulp.task('js', () => {
   gulp.src('src/**/*.{js,jsx}')
     .pipe(babel({
       presets: ['react', 'latest', 'stage-3']
     }))
     .pipe(gulp.dest(CONFIG.dist));
 });
+
+/* Final tasks */
+
+gulp.task('dist', ['clean', 'js', 'html']);
 
 gulp.task('dev', () => {
   gulp.watch('src/**/*.{js,jsx}', ['dist']);
