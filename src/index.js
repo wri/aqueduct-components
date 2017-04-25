f29acceab39313b4c89ffd72a3b7d65d9d5792c3
Andrés González Muñoz <andres.gnlez@gmail.com>
2017-04-25 15:55:18 +0200

    Adds Sticky component

--- src/index.js
+++ src/index.js
@@ -15,6 +15,12 @@ export { default as Map } from './components/Map';
 export { default as Accordion } from './components/ui/Accordion';
 export { default as Checkbox } from './components/ui/Checkbox';
 export { default as CheckboxGroup } from './components/ui/CheckboxGroup';
+export { default as Sticky } from './components/ui/Sticky';
+
+// Modal
+export { default as Modal } from './components/ui/Modal';
+export { default as InfoModal } from './components/ui/Modal/InfoModal';
+
 export { default as CustomSelect } from './components/ui/CustomSelect';
 export { default as DropdownButton } from './components/ui/DropdownButton';
 export { default as Header } from './components/ui/Header';
@@ -26,8 +32,9 @@ export { default as OnlyOn } from './components/ui/Responsive';
 export { default as Radio } from './components/ui/Radio';
 export { default as RadioGroup } from './components/ui/RadioGroup';
 export { default as SegmentedUi } from './components/ui/SegmentedUi';
-export { default as Spinner } from './components/ui/Spinner';
 export { default as Sidebar } from './components/ui/Sidebar';
+export { default as Spinner } from './components/ui/Spinner';
+export { default as Sticky } from './components/ui/Sticky';
 export { default as Timeline } from './components/ui/Timeline';
 export { default as ZoomControl } from './components/ui/ZoomControl';
 
