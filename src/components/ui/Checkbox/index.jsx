9152b62d5b66f5aa979b599598d1028d0283ffe9
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2017-05-12 14:34:43 +0200

    Disabled behaviour for radio, checkbox and timeline

--- src/components/ui/Checkbox/index.jsx
+++ src/components/ui/Checkbox/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Icon from '../Icon';
+import classnames from 'classnames';
 
 export default class Checkbox extends React.Component {
 
@@ -19,18 +20,21 @@ export default class Checkbox extends React.Component {
   }
 
   render() {
-    const { value, name, label, checked, defaultChecked, className } = this.props;
-    const cNames = ['c-checkbox'];
-    if (className) {
-      cNames.push(className);
-    }
+    const { value, name, label, checked, defaultChecked, disabled } = this.props;
+
+    const className = classnames({
+      [this.props.className]: !!this.props.className,
+      '-disabled': disabled
+    });
+
     return (
-      <div className={cNames.join(' ')}>
+      <div className={`c-checkbox ${className}`}>
         <input
           type="checkbox"
           name={name}
           id={`checkbox-${name}-${value}`}
           value={value}
+          disabled={disabled}
           defaultChecked={defaultChecked}
           checked={checked}
           onChange={this.onChange}
@@ -53,5 +57,6 @@ Checkbox.propTypes = {
   className: React.PropTypes.string,
   checked: React.PropTypes.bool,
   defaultChecked: React.PropTypes.bool,
+  disabled: React.PropTypes.bool,
   onChange: React.PropTypes.func
 };
