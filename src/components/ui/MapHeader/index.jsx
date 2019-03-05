3dc9b18146c5c43fae46475abedcee7c7fe99603
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2019-03-05 12:55:58 +0100

    Restores some components

--- src/components/ui/MapHeader/index.jsx
+++ src/components/ui/MapHeader/index.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-<<<<<<< HEAD
-=======
-import isEqual from 'lodash/isEqual';
->>>>>>> parent of 36e6f19... Removes map-related components
 import { substitution } from '../../../utils/text';
 
 export default class MapHeader extends React.Component {
