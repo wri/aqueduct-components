e1772fd40bb1cbb1781bbc268ae1462782d63388
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2019-05-23 16:42:32 +0200

    Adds tooltip to timeline component

--- src/components/ui/Timeline/index.jsx
+++ src/components/ui/Timeline/index.jsx
@@ -1,45 +1,89 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import Tooltip from 'rc-tooltip';
 
-export default function Timeline(props) {
-  const { items, selected } = props;
+class Timeline extends PureComponent {
+  render() {
+    const {
+      items,
+      selected,
+      onChange,
+      className,
+      disabled,
+      tooltipProps
+    } = this.props;
 
-  const classList = classnames('c-timeline', {
-    [props.className]: props.className,
-    '-disabled': props.disabled
-  });
+    const { overlay, ...restTooltipProps } = tooltipProps;
 
-  return (
-    <div className={classList}>
-      <ul className="timeline-list">
-        {items.map((item) => {
-          const itemClassList = classnames('timeline-list-item', {
-            '-selected': selected && (selected.value === item.value),
-            '-disabled': item.disabled
-          });
+    const classList = classnames('c-timeline', {
+      [className]: className,
+      '-disabled': disabled
+    });
 
-          return (
-            <li
-              key={item.value}
-              className={itemClassList}
-              onClick={() => !props.disabled && props.onChange && props.onChange(item)}
-            >
-              <div className="timeline-label">
-                {item.label}
-              </div>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
+    return (
+      <div className={classList}>
+        <ul className="timeline-list">
+          {items.map((item) => {
+            const itemClassList = classnames('timeline-list-item', {
+              '-selected': selected && (selected.value === item.value),
+              '-disabled': item.disabled
+            });
+
+            if (Object.keys(tooltipProps).length) {
+              return (
+                <Tooltip
+                  key={item.value}
+                  {...restTooltipProps}
+                  {...overlay && { overlay: overlay(item) }}
+                >
+                  <li
+                    key={item.value}
+                    className={itemClassList}
+                    onClick={() => !disabled && onChange && onChange(item)}
+                  >
+                    {item.label && (
+                      <div className="timeline-label">
+                        {item.label}
+                      </div>)}
+                  </li>
+                </Tooltip>
+              );
+            }
+
+            return (
+              <li
+                key={item.value}
+                className={itemClassList}
+                onClick={() => !disabled && onChange && onChange(item)}
+              >
+                <div className="timeline-label">
+                  {item.label}
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
+
+export default Timeline;
+
 
 Timeline.propTypes = {
   items: PropTypes.array.isRequired,
   selected: PropTypes.any.isRequired,
+  tooltipProps: PropTypes.object,
   disabled: PropTypes.bool,
   onChange: PropTypes.func,
   className: PropTypes.string
+};
+
+Timeline.defaultProps = {
+  className: null,
+  onChange: null,
+  disabled: false,
+  tooltipProps: {}
 };
