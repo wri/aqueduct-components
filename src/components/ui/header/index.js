150badd51ffc76919bed13392759bba50eb0ec28
Andrés González <andres.gonzalez@vizzuality.com>
2019-08-05 20:45:56 +0200

    Removes tools menu

--- src/components/ui/header/index.js
+++ src/components/ui/header/index.js
@@ -1,71 +1,46 @@
-import React, { PureComponent, Fragment } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-
-// constants
-import { TOOLS } from 'components/ui/header/tools/constants';
-
-// components
-import Icon from 'components/ui/icon';
-import ToolsMenu from './tools';
-
-// statics
-import logoImage from './logo.png';
 
 // styles
 import './styles.scss';
 
 class Header extends PureComponent {
   static propTypes = {
-    app: PropTypes.string.isRequired,
+    appRoute: PropTypes.string.isRequired,
+    logoRoute: PropTypes.string,
     title: PropTypes.string.isRequired,
     embed: PropTypes.bool,
     children: PropTypes.any.isRequired
   }
 
-  static defaultProps = { embed: false }
-
-  state = { tools: false }
-
-  onClickTools = () => {
-    this.setState({ tools: !this.state.tools });
+  static defaultProps = {
+    embed: false,
+    logoRoute: 'https://rawcdn.githack.com/Vizzuality/aqueduct-water-risk/f84043796b88afd1903022b1eb1047b20fb26f07/public/images/logo.png'
   }
 
   render() {
-    const { app, title, embed, children } = this.props;
-    const toolsButtonClass = classnames({ '-active': this.state.tools });
+    const { title, embed, children, appRoute, logoRoute } = this.props;
     const componentClass = classnames(
       'c-header',
       { '-embed': embed }
     );
 
-    const currentApp = TOOLS.find(_tool => _tool.app === app);
-
     return (
       <header styleName={componentClass} role="banner">
         <h1 styleName="c-header-logo">
-          <a href={`${embed ? currentApp.route : '/'}`} styleName="header-logo" style={{ backgroundImage: `url(${logoImage})` }}><span>Logo</span></a>
-          <a href={`${embed ? currentApp.route : '/'}`} styleName="header-title">{title}</a>
+          <a href={appRoute} styleName="header-logo" style={{ backgroundImage: `url(${logoRoute})` }}><span>Logo</span></a>
+          <a href={appRoute} styleName="header-title">{title}</a>
         </h1>
-        {!embed &&
-          <Fragment>
-            <nav role="navigation">
-              <ul styleName="list">
-                <li>
-                  <button styleName={toolsButtonClass} onClick={this.onClickTools}>
-                    <span>Tools</span>
-                  </button>
-                </li>
-                <li>
-                  {children}
-                </li>
-              </ul>
-            </nav>
-            <ToolsMenu
-              visible={this.state.tools}
-              currentApp={app}
-            />
-          </Fragment>}
+        {!embed && (
+          <nav role="navigation">
+            <ul styleName="list">
+              <li>
+                {children}
+              </li>
+            </ul>
+          </nav>
+        )}
       </header>
     );
   }
--- src/components/ui/header/tools/constants.js
+++
@@ -1,8 +0,0 @@
-export const TOOLS = [
-  { label: 'Water and Food Security Analyzer', route: 'https://wri.org/applications/aqueduct/water-food-security/', app: 'water-and-food-security-analyzer' },
-  { label: 'Risk Atlas', route: 'https://wri.org/applications/aqueduct/water-risk-atlas', app: 'water-risk-atlas' },
-  { label: 'Flood Risk Analyzer', route: 'https://wri.org/applications/aqueduct/flood', app: 'flood-risk-analyzer' },
-  { label: 'Country basin risk profiles and Rankings', route: 'https://wri.org/applications/aqueduct/country-rankings', app: 'country-basin-risk-profiles-and-rankings' }
-];
-
-export default { TOOLS };
