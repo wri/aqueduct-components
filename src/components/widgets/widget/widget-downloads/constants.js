0102177af1f02769d5c9d2c8a7fbacef4e6c9e95
Carlos Del Real <carloshdelreal@gmail.com>
2024-06-20 01:19:38 -0500

    feat: get rid of report download

--- src/components/widgets/widget/widget-downloads/constants.js
+++ src/components/widgets/widget/widget-downloads/constants.js
@@ -2,8 +2,7 @@ export const WIDGET_DOWNLOAD_OPTIONS = [
   { label: 'Embed widget', value: 'embed' },
   { label: 'Download CSV', value: 'csv' },
   { label: 'Downlaod JSON', value: 'json' },
-  { label: 'Download image', value: 'image' },
-  { label: 'Download report', value: 'report' }
+  { label: 'Download image', value: 'image' }
 ];
 
 export default { WIDGET_DOWNLOAD_OPTIONS };
